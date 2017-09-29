a4e77a4f25c18887ecafe1382c6ec64728b7a27a
Heinrich Schuchardt <xypron.glpk@gmx.de>
2017-09-29 23:55:10 +0200

    linux-image-4.13.0-trunk-arm64 (4.13.2-1~exp1)
    Use experimental kernel
    
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 apt-get update
 apt-get install less locales ssh sudo vim -y
 apt-get install u-boot-rockchip flash-kernel -y
+cd /tmp
+wget http://ftp.us.debian.org/debian/pool/main/l/linux/linux-image-4.13.0-trunk-arm64_4.13.2-1~exp1_arm64.deb
+dpkg -i linux-image-4.13.0-trunk-arm64_4.13.2-1~exp1_arm64.deb
 adduser firefly --uid 9997 --disabled-password --gecos 'Default User,,,'
 echo firefly:firefly | chpasswd
 adduser firefly sudo
